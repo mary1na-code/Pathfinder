595d22db719447910c0ec9afd9cb1a5d65952d81
elenhpapadima <elenhpapadima@gmail.com>
2026-02-06 15:38:36 +0200

    feat: integrate Tailwind CSS and refactor Roles component
    - Added Tailwind CSS dependencies to package.json
    - Imported Tailwind CSS in index.css
    - Refactored Roles component to use buttons for role selection instead of a dropdown
    - Implemented state management for dropdown visibility in Roles component
    - Updated Vite configuration to include Tailwind CSS plugin

--- src/pages/Roles.jsx
+++ src/pages/Roles.jsx
@@ -1,44 +1,42 @@
-import Header from "../components/Header.jsx"
-import Footer from "../components/Footer.jsx"
+import Header from "../components/Header.jsx";
+import Footer from "../components/Footer.jsx";
 import { roles } from "../data/roles.js";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Roles() {
+  const navigate = useNavigate();
 
-    const navigate= useNavigate()
-    
-    
-    
-    const handleChange = (e) => {
-        const roleId= e.target.value;
-        if (roleId) {
-            navigate(`/questions/${roleId}`) 
-        }
-    };
+  const [isOpen, setIsOpen] = useState(false);
 
-    
+  const handleChange = (id) => {
+    if (id) {
+      navigate(`/questions/${id}`);
+    }
+  };
 
-    return (          
-            <>
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
 
-                <Header/>            
+  return (
+    <>
+      <Header />
 
-        <select className ="roleslist" defaultValue="" onChange= {handleChange}>
-            <option value="" disabled>Get Started</option>
-
-            {roles.map(role => (
-            <option key={role.id} value={role.id}>
-               {role.label}
-            </option>
+      <div className="roleslist" defaultValue="">
+        <button onClick={handleClick}>Get Started</button>
+        {isOpen && (
+          <div>
+            {roles.map((role) => (
+              <button key={role.id} onClick={()=>handleChange(role.id)}>
+                {role.label}
+              </button>
             ))}
-        </select>
+          </div>
+        )}
+      </div>
 
-            <Footer/>
-        
-        
-        </>
-        
-        
-       
-    )
-};
+      <Footer />
+    </>
+  );
+}
